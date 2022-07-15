55f134b10a6e79f6e24bcc358d7200b72e006cb2
H Saligan C <betosach@gmail.com>
2022-07-14 20:45:24 -0500

    Update script.js
    Set to my coordinates

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function iniciarMap(){
-    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
+function initMap(){
+    var coordinates = {
+        lat:parseFloat(19.3416551), 
+        lng:parseFloat(-98.985995)
+        };
+    var map = new google.maps.Map(
+        document.getElementById('map'),{
+            zoom:10,
+            center:coordinates,
+            map:map            
+        });
+        var marker = new google.maps.Marker({
+            position:coordinates,
+            map:map
+        });
 }
